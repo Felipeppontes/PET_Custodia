cbb3dbd7299eed90a912c6c7192fe3629203c771
Felipe Pontes <107514967+Felipeppontes@users.noreply.github.com>
2024-04-14 12:31:38 -0300

    Add files via upload

---
+++ botao_e_leds/botao_e_leds.ino
@@ -0,0 +1,28 @@
+// C++ code
+//
+int botao = 2;
+int led = 4;
+
+void setup()
+{
+  pinMode(led,OUTPUT);
+  pinMode(botao,INPUT);
+}
+
+void loop()
+{
+  digitalRead(botao);//Ler o pino do botão
+  
+  //Se a leitura do pino onde esta ligado o botão igual a 5v 
+  //escreva no pino onde esta o LED 5v(ou seja, acenda o led)
+  if(digitalRead(botao) == HIGH){
+  digitalWrite(led,HIGH);
+    delay(1000);
+    digitalWrite(led,LOW);
+    delay(1000);
+  }else{ // Se a leitura do botão ele não estiver apertado não o forneça 5v.
+  digitalWrite(led, LOW);
+  }
+ 
+ 
+}
