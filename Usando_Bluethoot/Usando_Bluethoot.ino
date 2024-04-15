420a77b0bfba29faa0c6e552c9676cd9a8fe0cc5
Felipe Pontes <107514967+Felipeppontes@users.noreply.github.com>
2024-04-14 22:29:21 -0300

    Update Usando_Bluethoot.ino

--- Usando_Bluethoot/Usando_Bluethoot.ino
+++ Usando_Bluethoot/Usando_Bluethoot.ino
@@ -3,34 +3,24 @@
 // Na aba Devices Selecionamos a opção (bluetooth led) que é o nosso esp32.
 
 #include "BluetoothSerial.h"
-
-
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run 'make menuconfig' to and enable it
 #endif
-
 BluetoothSerial SerialBT; 
-
 char valorRecebido;
 
 void setup() {
-  
   Serial.begin(115200);
   SerialBT.begin("bluetooth led"); // Dando o Nome da placa esp32 que estará disponível para os outros dispositívos.  
   Serial.println("Dispositivo visível");
-
   pinMode(13,OUTPUT);
 }
-
 void loop() {
     valorRecebido =(char)SerialBT.read();
-
   if(Serial.available()){
     SerialBT.write(Serial.read());
-
   }
   if(SerialBT.available()){
-
     if(valorRecebido == '1'){
       SerialBT.println("LED 1 ligado:");
       digitalWrite(13,HIGH);
@@ -40,6 +30,4 @@ void loop() {
       digitalWrite(13,LOW);
     }
   }
-  delay(50);
-
-}
+  delay(50);}
